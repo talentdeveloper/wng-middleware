26f460d636debf227f3f48b48a456a13318dae61
Martijn Buurman <martijn@m19.nl>
2016-06-27 09:38:48 +0800

    Add host to db

--- src/database.js
+++ src/database.js
@@ -1,10 +1,13 @@
 import Sequelize from 'sequelize'
 const db = {
+  host: process.env.DB_HOST || '127.0.0.1',
   name: process.env.DB_NAME || 'user-management',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || ''
 }
-const sequelize = new Sequelize(db.name, db.user, db.password)
+const sequelize = new Sequelize(db.name, db.user, db.password, {
+  host: db.host
+})
 sequelize.sync()
 
 export const Account = sequelize.define('account', {
